a2d43e88a359e0501895eb48de624097cfab7321
Alessandro Aldieri <38555800+data-studio-seo@users.noreply.github.com>
2019-03-27 17:30:00 +0100

    Update alex-spider.py

--- books/spiders/alex-spider.py
+++ books/spiders/alex-spider.py
@@ -1,22 +1,29 @@
-# -*- coding: utf-8 -*-
+import urlparse
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+
+from scrapy.http import Request
 
 
 class BooksSpider(scrapy.Spider):
-    name = "Alex-Bot"
-    allowed_domains = ["https://www.traghettilines.it/"]
-    start_urls = [
+    name = "basic"
+    allowed_domains = ["traghettilines"]
+    #                 ^ allowed domain should be name of domain that you wanna scrap
+    start_urls = (
         'https://www.traghettilines.it/',
-    ]
+    )
 
-    def parse_book_page(self, response):
-        item = {}
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "/html/body/div[2]/div/div[1]/ol/span/span/a"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
-        yield item
+    def parse(self, response):
+    # My Link Extractor
+        next_page_urls = LinkExtractor(restrict_xpaths='//*[@class="next"]').extract_links(response)
+        # This is how we use LinkExtractor or you can create spider Rule for next page.
+        # Read more about LinkExtractor form https://doc.scrapy.org/en/latest/topics/link-extractors.html
+        for next_page in next_page_urls:
+            yield Request(next_page.url,callback=self.parse_item)
+
+    def parse_item(self, response):
+    # My Page Saver    
+        filename = response.url.split("/")[-1] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+            return
