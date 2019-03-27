c2d1b1078381b8a2c3ffe2595533de4f90e79b27
Alessandro Aldieri <38555800+data-studio-seo@users.noreply.github.com>
2019-03-27 17:14:39 +0100

    Update alex-spider.py

--- books/spiders/alex-spider.py
+++ books/spiders/alex-spider.py
@@ -3,18 +3,28 @@ import scrapy
 
 
 class BooksSpider(scrapy.Spider):
-    name = "Alex-tool"
-    allowed_domains = ["https://www.traghettiper-isole-golfo-napoli.it/"]
-    start_urls = ['https://www.traghettiper-isole-golfo-napoli.it',]
-    
-rules = (
-        Rule(LinkExtractor(allow=('tp-magazine', ), deny=('subsection\.php', ))),
-    )
-def parse_items(self, response):
-        item = UrlscraperItem()
-        item['url'] = response.url
-        item['status'] = response.status
-        item['referer'] = response.request.headers.get('Referer', None)
-        item['title'] = response.xpath('/html/head/title/text()').extract()
-        item['description'] = response.xpath('/html/head/meta[@name="description"]/@content').extract()
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = [
+        'http://books.toscrape.com/',
+    ]
+
+    def parse(self, response):
+        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
+            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
+        next_page = response.css("li.next > a ::attr(href)").extract_first()
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
+    def parse_book_page(self, response):
+        item = {}
+        product = response.css("div.product_main")
+        item["title"] = product.css("h1 ::text").extract_first()
+        item['category'] = response.xpath(
+            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
+        ).extract_first()
+        item['description'] = response.xpath(
+            "//div[@id='product_description']/following-sibling::p/text()"
+        ).extract_first()
+        item['price'] = response.css('p.price_color ::text').extract_first()
         yield item
